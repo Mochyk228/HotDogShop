8e841d3558a11d7ff5917c34ea0b52757079c56e
Mochyk228 <muravskiy.bogdan@icloud.com>
2025-03-26 09:51:09 +0000

    Fist 26.05

--- Scenes/character_body_3d.gd
+++ Scenes/character_body_3d.gd
@@ -1,13 +1,20 @@
 extends CharacterBody3D
 
-@onready var ray: RayCast3D = $RayCast3D
+@onready var ray: RayCast3D = $Camera3D/RayCast3D
 
 @export var rotation_add : float
+@export var sound : AudioStream
 var old_rot : float
 
 var animation : AnimationPlayer
-var is_open : bool
 var is_rot_left : bool
+
+var is_jump_1 : bool
+var is_jump_2 : bool
+
+var item_count : int = 0
+var pitch : float = 0.5
+var can_play : bool
 
 @onready var camera: Camera3D = $Camera3D
 
@@ -16,24 +23,51 @@ const SPEED = 0.5
 const JUMP_VELOCITY = 4.5
 
 func ray_cast():
-	var global = get_world_3d()
-	var ray_start = camera.project_ray_origin(mouse_get_position())
-
+	var global = get_world_3d().direct_space_state
+	var mouse_pos = get_viewport().get_mouse_position()
+	var ray_start = camera.project_ray_origin(mouse_pos)
+	var ray_end = ray_start + camera.project_ray_normal(mouse_pos) * 1000
+	var ray_query = PhysicsRayQueryParameters3D.create(ray_start, ray_end)
+	var ray_cast = global.intersect_ray(ray_query)
+	
+	if ray_cast and ray_cast.collider.is_in_group("Interactable"):
+		var object = ray_cast.collider
+		var children = $"../CanvasLayer/HBoxContainer".get_children()
+		children[item_count].visible = true
+		item_count += 1
+		pitch += 0.4
+		object.queue_free()
+		$PickUpSFX.pitch_scale = pitch
+		$PickUpSFX.play()
 
 func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("left_mouse_click"):
+		ray_cast()
+	
 	if is_rot_left:
 		rotation_degrees.y = clampf(rotation_add + old_rot, old_rot, old_rot + 90)
 	else:
 		rotation_degrees.y = clampf(rotation_add + old_rot, old_rot - 90, old_rot)
 	
-	if ray.is_colliding() and ray.get_collider().is_in_group("Fridge") and not is_open:
-		animation = ray.get_collider().get_child(2)
-		if not animation.is_playing():
-			animation.play("open_door")
-			is_open = true
-	elif not ray.is_colliding() and animation and not animation.is_playing() and is_open:
+	
+	if ray.is_colliding() and ray.get_collider().is_in_group("Fridge"):
+		if ray.is_colliding() and ray.get_collider().is_in_group("Fridge") and not is_jump_1:
+			animation = ray.get_collider().get_child(2)
+			if not animation.is_playing():
+				animation.play("open_door")
+				is_jump_1 = true
+	elif ray.is_colliding() and ray.get_collider().is_in_group("Door"):
+		if ray.is_colliding() and ray.get_collider().is_in_group("Door") and not is_jump_2:
+			animation = ray.get_collider().get_child(2)
+			if not animation.is_playing():
+				animation.play("open_door")
+				is_jump_2 = true
+	elif not ray.is_colliding() and animation and not animation.is_playing() and is_jump_1: 
 		animation.play("close_door")
-		is_open = false
+		is_jump_1 = false
+	elif ray.is_colliding() and animation and not animation.is_playing() and is_jump_2:
+		animation.play("close_door")
+		is_jump_2 = false
 	
 	# Add the gravity.
 	if not is_on_floor():
@@ -58,12 +92,18 @@ func _physics_process(delta: float) -> void:
 			rotation_add = 0
 			$AnimationPlayer.play("turn_right")
 			direction = Vector3.ZERO
+			ray.visible = false
+			await get_tree().create_timer(0.2)
+			ray.visible = true
 		if Input.is_action_just_pressed("ui_left"):
 			is_rot_left = true
 			old_rot = rotation_degrees.y
 			rotation_add = 0
 			$AnimationPlayer.play("turn_left")
 			direction = Vector3.ZERO
+			ray.visible = false
+			await get_tree().create_timer(0.2)
+			ray.visible = true
 	
 	
 	if direction:
@@ -77,4 +117,20 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 
+func _process(delta):
+	if velocity.length() > 0.1:
+		$AnimationPlayer2.play("walk")
+		if not can_play:
+			can_play = true
+			play_sound()
+			await get_tree().create_timer(0.4).timeout
+			can_play = false
+	else:
+		$AnimationPlayer2.stop()
+
+func play_sound():
+	$FootSound.stream = sound
+	$FootSound.max_db = 0 + randi_range(-5, -2)
+	$FootSound.pitch_scale = 1 + randf_range(-0.5, 0.5)
+	$FootSound.play()
 	
