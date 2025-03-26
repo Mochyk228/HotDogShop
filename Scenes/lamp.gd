09b863c6c8f1274a9c5e7b3761844b494773ebf3
Mochyk228 <muravskiy.bogdan@icloud.com>
2025-03-26 21:42:57 +0000

    26.03 Forth

--- Scenes/lamp.gd
+++ Scenes/lamp.gd
@@ -5,9 +5,9 @@ var is_started : bool
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _ready():
 	while true:
-		if not is_started:
+		if not is_started and not $AnimationPlayer.is_playing():
 			is_started = true
-			var rand_time = randi_range(10, 300)
+			var rand_time = randi_range(40, 120)
 			await get_tree().create_timer(rand_time).timeout
-			print(1)
+			$AnimationPlayer.play("flick")
 			is_started = false
