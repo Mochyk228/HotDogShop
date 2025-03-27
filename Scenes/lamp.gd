9cde4fc895c17b663c4c966b641830673beeaa4c
Mochyk228 <muravskiy.bogdan@icloud.com>
2025-03-27 10:08:32 +0000

    Pre last

--- Scenes/lamp.gd
+++ Scenes/lamp.gd
@@ -4,10 +4,17 @@ var is_started : bool
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _ready():
+	randomize()
+	# as fast and as much info as posiible so it is better no matter code or not.
+	start_flicker()
+
+func start_flicker():
 	while true:
-		if not is_started and not $AnimationPlayer.is_playing():
+		if not is_started:
 			is_started = true
-			var rand_time = randi_range(40, 120)
-			await get_tree().create_timer(rand_time).timeout
+			var rand_end = randi_range(20, 40)
+			var rand_time = randi_range(10, rand_end)
+			print(rand_time * 3)
+			await get_tree().create_timer(rand_time * 3).timeout
 			$AnimationPlayer.play("flick")
 			is_started = false
---
+++ Scenes/start.gd
@@ -0,0 +1,20 @@
+extends Camera3D
+
+@export var is_translate : bool
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if is_translate:
+		$"../CanvasLayer/Black".visible = true
+		$"../CharacterBody3D".visible = true
+		await get_tree().create_timer(2).timeout
+		$"../CanvasLayer/Black".visible = false
+		queue_free()
+	else:
+		$"../CharacterBody3D".visible = false
